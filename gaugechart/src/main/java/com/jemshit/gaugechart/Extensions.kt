1657b629ed64099f20b47c3afafd443edc2723ee
Jemshit Iskenderov <jemshit@mail.com>
2023-09-12 09:11:33 +0500

    * still trying to fix jitpack

--- gaugechart/src/main/java/com/jemshit/gaugechart/Extensions.kt
+++ gaugechart/src/main/java/com/jemshit/gaugechart/Extensions.kt
@@ -4,12 +4,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.luminance
 
 
-internal typealias Degree = Double
-internal typealias Radian = Double
+typealias Degree = Double
+typealias Radian = Double
 
 fun Degree.toRadian(): Radian = this / 180 * Math.PI
 
-internal fun Color.generateOnColor()
+fun Color.generateOnColor()
         : Color {
     return if (luminance() > 0.5f) {
         Color.Black.copy(alpha = .8f)
